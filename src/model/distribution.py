123fc2ea0d6a40ebde8a4b31cf1b66a85f5ce989
weikailee <william7699@gmail.com>
2024-06-08 07:52:31 +0800

    feat: change Ef derivative calculation method

--- src/model/distribution.py
+++ src/model/distribution.py
@@ -10,8 +10,7 @@ def d_fermi_dirac_distribution_dE(E: np.ndarray, Ef: float, kT: float) -> np.nda
     return -1 / (kT * (np.exp((E - Ef) / kT) + 1) ** 2)
 def d_fermi_dirac_distribution_dEf(E: np.ndarray, Ef: float, kT: float) -> np.ndarray:
     """ Derivative of Fermi-Dirac distribution function with respect to Fermi energy """
-    return 1 / (kT * (np.exp((E - Ef) / kT) + 1) ** 2)
-
+    return -d_fermi_dirac_distribution_dE(E, Ef, kT)
 
 """ bose einstein """
 def bose_einstein_distribution(E: np.ndarray, Ef: float, kT: float) -> np.ndarray:
@@ -22,7 +21,7 @@ def d_bose_einstein_distribution_dE(E: np.ndarray, Ef: float, kT: float) -> np.n
     return -1 / (kT * (np.exp((E - Ef) / kT) - 1) ** 2)
 def d_bose_einstein_distribution_dEf(E: np.ndarray, Ef: float, kT: float) -> np.ndarray:
     """ Derivative of Bose-Einstein distribution function with respect to Fermi energy """
-    return 1 / (kT * (np.exp((E - Ef) / kT) - 1) ** 2)
+    return -d_bose_einstein_distribution_dE(E, Ef, kT)
 
 """ maxwell boltzmann """
 def maxwell_boltzmann_distribution(E: np.ndarray, Ef: float, kT: float) -> np.ndarray:
@@ -33,7 +32,7 @@ def d_maxwell_boltzmann_distribution_dE(E: np.ndarray, Ef: float, kT: float) -> 
     return -1 / kT * np.exp(-(E - Ef) / kT)
 def d_maxwell_boltzmann_distribution_dEf(E: np.ndarray, Ef: float, kT: float) -> np.ndarray:
     """ Derivative of Maxwell-Boltzmann distribution function with respect to Fermi energy """
-    return 1 / kT * np.exp(-(E - Ef) / kT)
+    return -d_maxwell_boltzmann_distribution_dE(E, Ef, kT)
 
 
 
